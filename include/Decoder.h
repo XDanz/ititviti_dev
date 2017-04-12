84f91bf5b7f8eaecc07803183e65b42325f3168e
Daniel Terranova <daniel.terranova@gmail.com>
2017-04-12 05:33:51 +0200

    Docs

--- include/Decoder.h
+++ include/Decoder.h
@@ -1,16 +1,44 @@
-//
-// Created by danter on 2017-04-10.
-//
-
+/**
+ * @file   Decoder.h
+ * @Author Daniel Terranova (daniel.terranova@gmail.com)
+ * @date   April, 2017
+ * @brief  File contains declaration of utility class.
+ *
+ * Detailed description of file.
+ */
 #ifndef ITIVITI_DEV_DECODER_H
 #define ITIVITI_DEV_DECODER_H
 
 #include <cstdint>
 
+//!  A class intend to decode big-endian array of size to unsigned type.
+/*!
+  A utility class to decode big-endian array of size to uint64_t.
+
+  The class contains only one member function
+*/
 class Decoder {
-// Warning:  Untested preconditions (e.g., 0 <= size <= 8)
+
 public:
-    uint64_t DecodeIntBigEndian(uint8_t val[], int offset, int size);
+  /**
+ * @name    Example API Actions
+ * @brief   Example actions available.
+ * @ingroup example
+ *
+ * This API provides utility member function
+ *
+ * @param [in] val array of bytes representing big-endian number of size .
+ * @param [in] offset (zero indexed) where to start decoding process, usually 0
+ *
+ * @retval uint64_t the decoded value
+ *
+ * Example Usage:
+ * @code
+ *   uint64_t ret = DecodeIntBigEndian(val,0,sizeof(uint64_t));
+ * @endcode
+ * // Warning:  Untested preconditions (e.g., 0 <= size <= 8)
+ */
+  uint64_t DecodeIntBigEndian(uint8_t val[], int offset, int size);
         
 };
 
--- include/Encoder.h
+++ include/Encoder.h
@@ -1,12 +1,22 @@
-//
-// Created by danter on 2017-04-10.
-//
-
+/**
+ * @file   Encoder.h
+ * @Author Daniel Terranova (daniel.terranova@gmail.com)
+ * @date   April, 2017
+ * @brief  File contains declaration of utility class.
+ *
+ * Detailed description of file.
+ */
 #ifndef ITIVITI_DEV_ENCODER_H
 #define ITIVITI_DEV_ENCODER_H
 #include <climits>
 #include <cstdint>
 
+//!  A class intend to encode unsigned type big-endian to array of size to unsigned type.
+/*!
+  A utility class to decode big-endian array of size to uint64_t.
+
+  The class contains only one member function
+*/
 class Encoder {
 public:
     // Warning:  Untested preconditions (e.g., 0 <= size <= 8)
